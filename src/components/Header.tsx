3f365387752d916c98b8358766ae86fdcab7a4aa
AchiGel <ar.gelashvili@gmail.com>
2025-09-19 12:19:38 +0400

    add header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="flex justify-between items-center px-25 py-[10px] h-20">
+      {/* Logo */}
+      <Link to={"/"}>
+        <div className="flex items-center gap-1">
+          <img src="/images/HandEye.svg" alt="Logo" />
+          <h2 className="font-semibold text-[#10151F] text-base">
+            RedSeam Clothing
+          </h2>
+        </div>
+      </Link>
+
+      <button className="flex items-center gap-2 font-medium text-[#10151F] text-xs cursor-pointer">
+        <img src="/images/Union.svg" alt="login" />
+        <span>Log in</span>
+      </button>
+    </header>
+  );
 };
 
 export default Header;
--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,5 +1,26 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <div className="flex">
+        <div className="max-w-[948px]">
+          <img
+            src="/images/authCover.png"
+            alt="Hero"
+            className="object-cover"
+          />
+        </div>
+        <div className="flex flex-1 justify-center items-center bg-gray-50 p-6">
+          <div className="bg-white shadow-md p-8 rounded-2xl w-full max-w-md">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AuthLayout;
