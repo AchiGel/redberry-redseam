019a8e1ddcc8a49dc13b07a0d30a8f4c81684726
AchiGel <ar.gelashvili@gmail.com>
2025-09-19 12:30:28 +0400

    add AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,20 +3,19 @@ import Header from "../components/Header";
 
 const AuthLayout = () => {
   return (
-    <div className="min-h-screen">
+    <div className="flex flex-col mx-auto max-w-[1920px] min-h-screen">
       <Header />
-      <div className="flex">
-        <div className="max-w-[948px]">
+
+      <div className="flex flex-1 h-full">
+        <div className="flex-1 max-w-[948px]">
           <img
             src="/images/authCover.png"
             alt="Hero"
-            className="object-cover"
+            className="w-full h-full object-cover"
           />
         </div>
-        <div className="flex flex-1 justify-center items-center bg-gray-50 p-6">
-          <div className="bg-white shadow-md p-8 rounded-2xl w-full max-w-md">
-            <Outlet />
-          </div>
+        <div className="flex flex-1 justify-center items-center">
+          <Outlet />
         </div>
       </div>
     </div>
